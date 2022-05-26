f51aec02fd97de95cdbab466f1099335acb9ceba
pjechris <pjechris@users.noreply.github.com>
2022-05-26 12:35:46 +0200

    [action] renamed action InputAction

--- Sources/AsyncUI/Action/InputAction+AnyObject.swift
+++ Sources/AsyncUI/Action/InputAction+AnyObject.swift
@@ -1,33 +1,26 @@
 import Foundation
 import Combine
 
-extension Action {
+extension InputAction {
     /// Create an action using an object function as the action
     ///
     /// Because we cannot reference functions using keypath you'll have to pass it as a static non instance function.
     /// For instance `MyViewViewModel.doSomething(_:)`
     ///
     /// - Parameter canExecute: a signal saying whether the action is enabled or not
-    /// - Parameter object: the object to execute the action on. Action do **NOT** keep a strong reference on it.
+    /// - Parameter on: the object to execute the action on. Action do **NOT** keep a strong reference on it.
+    /// - Parameter execute: the object method. For instance `MyViewViewModel.doSomething(_:)`
     public convenience init<T: AnyObject, P: Publisher, K: Publisher>(
-        object: T,
-        enabledIf: K,
+        on object: T,
+        canExecute: K,
         execute: @escaping (T) -> (Input) -> P
     ) where P.Output == Output, P.Failure == Error, K.Output == Bool, K.Failure == Never {
-        self.init(canExecute: enabledIf) { [weak object] input -> AnyPublisher<P.Output, P.Failure> in
+        self.init(canExecute: canExecute) { [weak object] input -> AnyPublisher<P.Output, P.Failure> in
             guard let object = object else {
                 return Empty().eraseToAnyPublisher()
             }
             
             return execute(object)(input).eraseToAnyPublisher()
         }
-    }
-    
-    public convenience init<T: AnyObject, P: Publisher, K: Publisher>(
-        object: T,
-        enabledIf enabledIfKeyPath: KeyPath<T, K>,
-        execute: @escaping (T) -> (Input) -> P
-    ) where P.Output == Output, P.Failure == Error, K.Output == Bool, K.Failure == Never {
-        self.init(object: object, enabledIf: object[keyPath: enabledIfKeyPath], execute: execute)
     }
 }
--- Sources/AsyncUI/Action/InputAction.swift
+++ Sources/AsyncUI/Action/InputAction.swift
@@ -1,14 +1,47 @@
 import Foundation
 import Combine
 
-public class Action<Input, Output>: ObservableObject {
+/// Represents a value that accept an action taking `Input` as its input and
+/// producing an `Output` when executed.
+///
+/// Example: Create an action running only if user has permission
+///
+/// ```swift
+/// class SomeClass: ObservableObject {
+///     var deleteNote: Action<Void, Void>!
+///     @Published var isUserAdmin = false
+///
+///     init() {
+///         self.deleteNote = Action(canExecute: $isUserAdmin) { [weak self] in
+///             self?.delete(note: $0)
+///         }
+///     }
+///
+///     func delete(note: Note) -> AnyPublisher<Void, Error> {
+///         /// delete the note
+///     }
+/// }
+/// ```
+///
+/// Alternatively you can also pass the object and its instance method in order to avoid using `weak` everytime:
+///
+/// ```swift
+///     self.deleteNote = Action(on: self, canExecute: $isUserAdmin, execute: SomeClass.deleteNote(note:))
+/// ```
+public class InputAction<Input, Output>: ObservableObject {
+    /// true if the action is can be executed. Has no effect if chaning *while* the action is already running
     @Published public private(set) var canExecute: Bool = false
+    /// true if the action is currently running
     @Published public private(set) var isExecuting: Bool = false
+    /// the last result from the action
     @Published public private(set) var result: Result<Output, Error>?
     
     private let action: (Input) -> AnyPublisher<Output, Error>
     private var cancellables: Set<AnyCancellable> = []
     
+    /// init the action by supplying when it is enabled or not and the closure to run
+    /// - Parameter canExecute: a publisher indicating whenever the action is enabled or not
+    /// - Parameter execute: the action itself to run
     public init<K: Publisher, P: Publisher>(canExecute: K, execute: @escaping (Input) -> P)
     where K.Output == Bool, K.Failure == Never, P.Output == Output, P.Failure == Error {
         
@@ -19,12 +52,15 @@ public class Action<Input, Output>: ObservableObject {
             .store(in: &cancellables)
     }
     
+    /// Convenience init to use when the action should always be enabled
+    /// - Parameter execute: the action itself to run
     public convenience init<P: Publisher>(execute: @escaping (Input) -> P) where P.Output == Output, P.Failure == Error {
         self.init(canExecute: Just(true).eraseToAnyPublisher(), execute: execute)
     }
-    
-    
-    
+
+    /// execute the action (if enabled) with given `input`.
+    ///
+    /// This is a special Swift method which can be called as regular function on the object.
     public func callAsFunction(_ input: Input) {
         guard canExecute && !isExecuting else {
             return
